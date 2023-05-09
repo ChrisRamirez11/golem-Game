433291f99402ace0426baccfd5450a535c712e33
KabraX <jerrydasher375@gmail.com>
2023-05-08 20:59:50 -0700

    Update
    Actualizacion de rutas y nombre de Main.tscn cambiado a Level1

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://scenes/Main.tscn")
+	get_tree().change_scene("res://scenes/Level1.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
