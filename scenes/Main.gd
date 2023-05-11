3b6252b83d7f36f724ba9f641abfb9c9986ed858
Chris Castañeda <chriscasezcacr@gmail.com>
2023-05-11 18:23:44 -0400

    trying

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,30 +1,7 @@
 extends Node2D
 
-var PauseMenu: String = "res://scenes/PauseMenu.tscn"
-var paused: Object = null
 
 func _ready() -> void:
 	OS.center_window()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$GUI/StatusGUI/Label2.set_text(String ($GoldChest.get_child_count()))
-
-<<<<<<< Updated upstream
-func _process(delta):
-	_on_PauseButton_pressed()
-	
 
 
-func _on_PauseButton_pressed():
-	if Input.is_action_pressed("ui_cancel"):
-		if paused == null:
-			paused = load(PauseMenu).instance()
-			$GUI/PauseScreen.add_child(paused)
-			paused.connect("nuller", self, "on_paused_quit")
-			get_tree().paused = true
-
-
-func on_paused_quit():
-	paused = null
-=======
-
->>>>>>> Stashed changes
--- scenes/Monolith.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_Player_monolith_activation():
-	$AnimatedSprite.play("getting_power")
-	
