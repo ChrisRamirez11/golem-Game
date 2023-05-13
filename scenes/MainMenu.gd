b9ff30f76ef02e13ab215983aa1385817cc88b51
Chris Castañeda <chriscasezcacr@gmail.com>
2023-05-13 17:02:45 -0400

    focus sound
    focus sound added to main menu

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var start_button: Button = $VBoxContainer/StartButton
+onready var focus_sound: AudioStreamPlayer2D = $focus_sound
 
 func _ready():
 	OS.center_window()
@@ -14,3 +15,11 @@ func _on_StartButton_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_StartButton_focus_exited() -> void:
+	focus_sound.play()
+
+
+func _on_QuitButton_focus_exited() -> void:
+	focus_sound.play()
