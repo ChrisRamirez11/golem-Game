0bdf3ea1fda965845442470172cc163bc34b7589
Chris Castañeda <chriscasezcacr@gmail.com>
2023-05-28 20:56:01 -0400

    errors fixed
    an error in cave activation code where when the player walk in monolith queue frees again the cave collision

--- scenes/ExitCave.gd
+++ scenes/ExitCave.gd
@@ -3,8 +3,10 @@ extends StaticBody2D
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
 
-
+var act_count:bool=false
 
 func _on_Player_monolith_activation() -> void:
-	animated_sprite.play("opening")
-	collision_polygon_2d.queue_free()
+	if act_count==false:
+		animated_sprite.play("opening")
+		collision_polygon_2d.queue_free()
+		act_count=true
